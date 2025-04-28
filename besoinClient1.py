6cf4c6576d4fbaca58d31b6f59217275d6e441ef
Allan Cueff <allan.cueff@protonmail.com>
2025-04-28 12:04:35 +0200

    script & carte beosin client 1

---
+++ besoinClient1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import joblib
+import numpy as np
+
+def main():
+    parser = argparse.ArgumentParser(description='Prends en entrée des caractéristiques d\'un arbre et renvoie le cluster de taille d\'arbre associé')
+    parser.add_argument('-n', '--clusters', type=str, help='Nombre de clusters', required=True)
+    parser.add_argument('-d', '--diam_tronc', type=str, help='Diametre du tronc', required=True)
+    parser.add_argument('-t', '--haut_tot', type=str, help='Hauteur totale de l\'arbre', required=True)
+
+    args = parser.parse_args()
+
+    filename = ""
+
+    if(args.clusters == "2"):
+        filename = "kmeans_haut_tot_from_diam_tronc_2_clusters.pkl"
+    elif(args.clusters == "3"):
+        filename = "kmeans_haut_tot_from_diam_tronc_3_clusters.pkl"
+    else:
+        return # error
+
+    kmeans_loaded = joblib.load(filename)
+
+    new_data = np.array([[float(args.haut_tot),float(args.diam_tronc)]])
+
+    predicted_cluster = kmeans_loaded.predict(new_data)
+
+    print(predicted_cluster)
+
+    print(f"Le modèle KMeans prédit que cet arbre est dans le cluster numéro {predicted_cluster[0]}")
+
+if __name__ == "__main__":
+    main()
--- map.py
+++ map.py
@@ -1,54 +1,31 @@
 import geopandas as gpd
 import folium
 from branca.colormap import LinearColormap
+import plotly.express as px
 
-def createDiscreteMap(dataframe, columnName, mapName, values = [], labels = [], colors = []):
+def createDiscreteMap(dataframe, columnName, values = [], colors = []):
     lambert = gpd.GeoDataFrame(dataframe, geometry=gpd.points_from_xy(dataframe.X, dataframe.Y), crs="EPSG:3949")
     wgs84 = lambert.to_crs(epsg=4326)
-    new_map = folium.Map(zoom_start = 13, tiles = 'cartodb.positron')
-    coord_list = [[point.xy[1][0], point.xy[0][0]] for point in wgs84.geometry]
-    i = 0
-    for coordinates in coord_list:
-        j = 0
-        for val in values:
-            if wgs84[columnName][i] == val:
-                new_map.add_child(
-                    folium.CircleMarker(
-                        location=coordinates,
-                        radius=2,
-                        weight=5,
-                        popup=labels[j],
-                        color=colors[j]
-                    )
-                )
-                break;
-            else:
-                j = j + 1 
-        i = i + 1
-
-    new_map.fit_bounds(new_map.get_bounds())
-    new_map.save(mapName + '.html')
+    wgs84[columnName] = wgs84[columnName].astype(str)
+    fig = px.scatter_map(wgs84,
+                         lat=wgs84.geometry.y,
+                         lon=wgs84.geometry.x,
+                         color=columnName,
+                         color_discrete_map={val: colors[i] for i, val in enumerate(values)},
+                         size_max=15,
+                         zoom=10)
+    fig.show() 
 
 
-def createContinuousMap(dataframe, columnName, mapName, minVal, maxVal, colors = []):
-    colormap = LinearColormap(colors=colors, vmin=minVal, vmax=maxVal)
+def createContinuousMap(dataframe, columnName, colors = []):
     lambert = gpd.GeoDataFrame(dataframe, geometry=gpd.points_from_xy(dataframe.X, dataframe.Y), crs="EPSG:3949")
     wgs84 = lambert.to_crs(epsg=4326)
-    new_map = folium.Map(zoom_start = 13, tiles = 'cartodb.positron')
-    coord_list = [[point.xy[1][0], point.xy[0][0]] for point in wgs84.geometry]
-    i = 0
-    for coordinates in coord_list:
-        new_map.add_child(
-            folium.CircleMarker(
-                location=coordinates,
-                radius=2,
-                weight=5,
-                popup=wgs84[columnName].iloc[i],
-                color=colormap(wgs84[columnName].iloc[i])
-            )
-        )
-        i = i + 1
-
-    new_map.add_child(colormap)
-    new_map.fit_bounds(new_map.get_bounds())
-    new_map.save(mapName + '.html')
+    wgs84[columnName] = wgs84[columnName].astype(float)
+    fig = px.scatter_map(wgs84,
+                         lat=wgs84.geometry.y,
+                         lon=wgs84.geometry.x,
+                         color=columnName,
+                         color_continuous_scale=colors,
+                         size_max=15,
+                         zoom=10)    
+    fig.show()
